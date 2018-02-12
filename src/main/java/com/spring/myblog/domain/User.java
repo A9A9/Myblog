81e50ee7ff8412a003070d1733b39e16ba8c27c7
sdh2398 <ehdgus2398@naver.com>
2018-02-13 03:26:33 +0900

    User add

--- src/main/java/com/spring/myblog/domain/User.java
+++ src/main/java/com/spring/myblog/domain/User.java
@@ -1,5 +1,39 @@
 package com.spring.myblog.domain;
 
 public class User {
-
+	private String userId;
+	private String userPw;
+	private String userName;
+	private String userEmail;
+	private Profile userProfile;
+	public String getUserId() {
+		return userId;
+	}
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+	public String getUserPw() {
+		return userPw;
+	}
+	public void setUserPw(String userPw) {
+		this.userPw = userPw;
+	}
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public String getUserEmail() {
+		return userEmail;
+	}
+	public void setUserEmail(String userEmail) {
+		this.userEmail = userEmail;
+	}
+	public Profile getUserProfile() {
+		return userProfile;
+	}
+	public void setUserProfile(Profile userProfile) {
+		this.userProfile = userProfile;
+	}
 }
