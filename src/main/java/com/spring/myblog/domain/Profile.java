a8060bb738821a38f16724ae4d939f25092cb518
MISEONGKIM <rlaaltjd46@naver.com>
2018-02-20 21:51:46 +0900

    root-context.xml

--- src/main/java/com/spring/myblog/domain/Profile.java
+++ src/main/java/com/spring/myblog/domain/Profile.java
@@ -1,48 +1,48 @@
 package com.spring.myblog.domain;
 
 public class Profile {
-	private String userID;
-	private String profilePhoto;
-	private String nickName;
-	private String profileIntro;
-	private String blogName;
-	private String blogVisibility;
-	
-	public String getUserID() {
-		return userID;
-	}
-	public void setUserID(String userID) {
-		this.userID = userID;
-	}
-	public String getProfilePhoto() {
-		return profilePhoto;
-	}
-	public void setProfilePhoto(String profilePhoto) {
-		this.profilePhoto = profilePhoto;
-	}
-	public String getNickName() {
-		return nickName;
-	}
-	public void setNickName(String nickName) {
-		this.nickName = nickName;
-	}
-	public String getProfileIntro() {
-		return profileIntro;
-	}
-	public void setProfileIntro(String profileIntro) {
-		this.profileIntro = profileIntro;
-	}
-	public String getBlogName() {
-		return blogName;
-	}
-	public void setBlogName(String blogName) {
-		this.blogName = blogName;
-	}
-	public String getBlogVisibility() {
-		return blogVisibility;
-	}
-	public void setBlogVisibility(String blogVisibility) {
-		this.blogVisibility = blogVisibility;
-	}
-	
+   private String userId;
+   private String profilePhoto;
+   private String nickName;
+   private String profileIntro;
+   private String blogName;
+   private String blogVisibility;
+   
+   public String getUserId() {
+      return userId;
+   }
+   public void setUserId(String userId) {
+      this.userId = userId;
+   }
+   public String getProfilePhoto() {
+      return profilePhoto;
+   }
+   public void setProfilePhoto(String profilePhoto) {
+      this.profilePhoto = profilePhoto;
+   }
+   public String getNickName() {
+      return nickName;
+   }
+   public void setNickName(String nickName) {
+      this.nickName = nickName;
+   }
+   public String getProfileIntro() {
+      return profileIntro;
+   }
+   public void setProfileIntro(String profileIntro) {
+      this.profileIntro = profileIntro;
+   }
+   public String getBlogName() {
+      return blogName;
+   }
+   public void setBlogName(String blogName) {
+      this.blogName = blogName;
+   }
+   public String getBlogVisibility() {
+      return blogVisibility;
+   }
+   public void setBlogVisibility(String blogVisibility) {
+      this.blogVisibility = blogVisibility;
+   }
+   
 }
